e5332575dfbcf23fff286faa77e3e44c5646b1f1
Ha Huynh <huynhsamha@gmail.com>
2019-08-06 08:56:56 +0700

    upload za 12

---
+++ za12/precache-manifest.335f9872d75aa4c316a95b0b73ef0e4c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1f74ab43958445ba182e32d3ca4bfe8",
+    "url": "/za12/index.html"
+  },
+  {
+    "revision": "e7d339bc7e154a996a8f",
+    "url": "/za12/static/css/main.80493ef5.chunk.css"
+  },
+  {
+    "revision": "3049e553f911fa5d6a4c",
+    "url": "/za12/static/js/2.ebd4a96a.chunk.js"
+  },
+  {
+    "revision": "e7d339bc7e154a996a8f",
+    "url": "/za12/static/js/main.7ca0e6ee.chunk.js"
+  },
+  {
+    "revision": "b137e631017118874274",
+    "url": "/za12/static/js/runtime~main.f619d941.js"
+  }
+]);
